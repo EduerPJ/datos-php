0b8e24c12f5c47159b3a3d47562f1803bdf0cf39
Eduer Pallares <45724815+EduerPJ@users.noreply.github.com>
2020-10-23 19:36:03 -0500

    Update README.md

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Manejo de Datos con PHP</title>
+  </head>
+  <body>
+    <main>
+      <h1>Manejo de Datos con PHP</h1>
+    </main>
+  </body>
+</html>
