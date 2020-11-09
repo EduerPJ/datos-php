d194491d082c53af1be3de6e7f5551f211b9d45a
EduerPJ <eduerpj@gmail.com>
2020-11-09 08:16:25 -0500

    sin resolver autoload

--- index.php
+++ index.php
@@ -1,13 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Manejo de Datos con PHP</title>
-  </head>
-  <body>
-    <main>
-      <h1>Manejo de Datos con PHP</h1>
-    </main>
-  </body>
-</html>
+<?php
+
+require __DIR__ . './vendor/autoload.php';
