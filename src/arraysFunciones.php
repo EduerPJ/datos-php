4c6772ce39f1da5682543966422073d299935505
EduerPJ <eduerpj@gmail.com>
2020-11-09 12:32:11 -0500

    manipulacion de arrays con funciones diff

--- src/arraysFunciones.php
+++ src/arraysFunciones.php
@@ -1,2 +1,27 @@
 <?php
 echo "<h3> Funciones para manipular Arrays </h3>";
+
+$courses = [
+    'php',
+    'javascript',
+    'laravel',
+    'symfony'
+];
+
+/*
+*sort($arry);
+*rsort($arry);
+*ksort($arry);
+*krsort($arry);
+*/
+
+// var_dump(array_slice($courses, 1));   // Inicia desde la key dada
+// var_dump(array_chunk($courses, 2));  // Agrupa por el número dado
+// var_duump(array_shift($courses));         // Elimina el primer elemento y lo retorna
+// var_duump(array_pop($courses));            // Elimina el último elemento y lo retorna
+array_unshift($courses, 'TypeScript');         // Asigna al primer key
+array_push($courses, 'node');                   // Agrega al final
+var_dump($courses);
+
+array_flip($courses);           // intercambiar el valor de la key por la del valor
+var_dump($courses);
