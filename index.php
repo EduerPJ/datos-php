f22414988c59a2d72b4b455fe1c870569587ca62
EduerPJ <eduerpj@gmail.com>
2020-11-09 12:11:18 -0500

    init funciones para arrays

--- index.php
+++ index.php
@@ -5,37 +5,3 @@ ini_set('display_starup_error', 1);
 error_reporting(E_ALL);
 
 require __DIR__. '/vendor/autoload.php';
-// var_dump(App\Validate::email('eduer@eduer.com'));
-
-\App\Argumentos::greet('Eduer Pallares');
-echo "<br>";
-
-\App\Argumentos::references($course);
-echo "<br>";
-
-\App\Argumentos::predeterminado('Samuel Pallares');
-echo "<br>";
-
-\App\Argumentos::predeterminado();
-echo "<br>";
-
-// Funciones anónimas
-if (! function_exists('saludando')) {
-    function saludando(Closure $lang, $name)
-    {
-        return $lang($name);
-    }
-}
-
-$es = function ($name) {
-    return "Hola, $name";
-};
-
-$en = function ($name) {
-    return "Hello, $name";
-};
-
-echo "<br>";
-echo saludando($es, 'Lynda');
-echo "<br>";
-echo saludando($en, 'Bella');
---
+++ src/arraysFunciones.php
@@ -0,0 +1,2 @@
+<?php
+echo "<h3> Funciones para manipular Arrays </h3>";
